801493421b78c4a03c7a60c0e96c3c2fd17e1bda
dtrajkoski <danieltrajkoski>
2025-01-29 22:19:29 +0100

    BattleRepository Interface wasnt pushed

--- src/main/java/sampleapp/persistence/repository/BattleRepository.java
+++ src/main/java/sampleapp/persistence/repository/BattleRepository.java
@@ -3,6 +3,6 @@ package sampleapp.persistence.repository;
 import java.sql.SQLException;
 
 public interface BattleRepository {
-    void updateStats(String username, boolean isWinner) throws SQLException;
+    void updateStats(String username, String result) throws SQLException;
     void updateCoins(String username) throws SQLException;
 }
